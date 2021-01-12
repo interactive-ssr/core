77ff89520c0235996d499bd5f20eb12c7ddb3eaf
Charles Jackson <cbj170230@utdallas.edu>
2021-01-12 17:53:52 -0600

    Use symbols instead of keywords for export

--- package.lisp
+++ package.lisp
@@ -1,14 +1,15 @@
 (defpackage issr-core
   (:use #:cl #:plump)
-  (:export :clients
-           :on-connect-hook
-           :on-disconnect-hook
-           :*ws-port*
-           :*socket*
-           :*first-time*
-           :*id*
-           :hash-keys
-           :generate-id
-           :clean
-           :diff-dom))
+  (:export
+   clients
+   on-connect-hook
+   on-disconnect-hook
+   *ws-port*
+   *socket*
+   *first-time*
+   *id*
+   hash-keys
+   generate-id
+   clean
+   diff-dom))
 
